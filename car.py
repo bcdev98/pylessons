4aed15334ea1739329abde0eae8359b5306e0acc
Brett Cui <brettcui@gmail.com>
2018-07-20 20:56:49 -0400

    adding car class

---
+++ car.py
@@ -0,0 +1,6 @@
+class Car():
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
---
+++ my_car.py
@@ -0,0 +1,3 @@
+import car
+
+my_beetle = car.Car("Volkswagen", "Beetle", 2016)
