76fc8dd0fb3b718478efd39c4d66172ec673a690
BC <brettcui@gmail.com>
2018-07-20 22:50:55 -0400

    update car class

--- car.py
+++ car.py
@@ -3,4 +3,36 @@ class Car():
         self.make = make
         self.model = model
         self.year = year
+        self.fuel_capacity = 15
+        self.fuel_level = 0
 
+    def fill_tank(self): 
+        self.fuel_level = self.fuel_capacity
+        print("Fuel tank is full")
+    
+    def add_fuel(self, amt):
+        if (amt > self.fuel_capacity):
+            self.fuel_level = self.fuel_capacity
+        else:
+            if (amt + self.fuel_level > self.fuel_capacity):
+                self.fuel_level = self.fuel_capacity
+                print("Fuel tank is filled up")
+            else:
+                self.fuel_level = amt + self.fuel_level
+                print("add fuel {}".format(self.fuel_level))
+
+    def drive(self):
+        print("Drive the car")
+    
+# inheritance
+class EletricCar(Car):
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+
+        # add aditional attributes
+        self.battery_size = 100
+        self.charge_level = 0
+    
+    def charge(self):
+        self.charge_level = 100
+        print("car is fully charged")
--- my_car.py
+++ my_car.py
@@ -1,3 +1,9 @@
 import car
 
-my_beetle = car.Car("Volkswagen", "Beetle", 2016)
+if __name__ == "__main__":
+    my_beetle = car.Car("Volkswagen", "Beetle", 2016)
+    my_telsa = car.EletricCar("Tesla", "S", 2018)
+    
+    my_beetle.add_fuel(10)
+    my_telsa.charge()
+    print(my_telsa.model)
